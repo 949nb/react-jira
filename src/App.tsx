d5b3263dd89cc6a5143bfe5a625e10f509192822
zhanghanyu <kazuki_99@outlook.com>
2021-08-12 15:38:39 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
     const {user} = useAuth()
     return (
         <div className="App">
-
             { user ? <AuthenticatedApp /> : <UnauthenticatedApp/> }
         </div>
     );
